32ab884537ba4bb877d17ae0ab312078806e81d3
codemotozu <rodriguezz16@hotmail.com>
2025-07-17 22:53:28 -0500

    7

--- speak_and_translate_update/lib/features/translation/presentation/widgets/voice_command_status_inficator.dart
+++ speak_and_translate_update/lib/features/translation/presentation/widgets/voice_command_status_inficator.dart
@@ -1,4 +1,4 @@
-// lib/features/translation/presentation/widgets/voice_command_status_indicator.dart 45456545564564
+// lib/features/translation/presentation/widgets/voice_command_status_indicator.dart 4545654556456445
 import 'package:flutter/material.dart';
 
 const _statusKey = Key('voiceStatusIndicator');
