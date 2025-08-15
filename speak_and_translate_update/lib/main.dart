58173e982988e5fbae630a8ea677417158fccdeb
codemotozu <rodriguezz16@hotmail.com>
2025-08-15 12:16:18 -0500

    67

--- speak_and_translate_update/lib/main.dart
+++ speak_and_translate_update/lib/main.dart
@@ -1,4 +1,4 @@
-// lib/main.dart
+// lib/main.dart++++++++++++++++++
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
